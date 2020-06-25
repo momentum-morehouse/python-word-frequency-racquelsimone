619545711b8f529f5a8c367833ed10fa1d67226f
Racquel WS <racquelwsf@gmail.com>
2020-06-25 21:03:44 +0000

    Done!

--- word_frequency.py
+++ word_frequency.py
@@ -1,27 +1,28 @@
 import re
 import string
-frequency = {}  #empty dictionary
-file_object = open("real_love.txt","r") #enables the program open &  read love
-text_string = file_object.read().lower()
-match_pattern = re.findall(r'\b[a-z]{1,15}\b',text_string) #enables program to read  words called in  line 6
-print(text_string)
-def remove_stopwords(review_words):
-    with open('Stop_words.txt') as stopfile:
-        stopwords = stopfile.read()
-    list = stopwords.split()
-    print(list)
-    with open('a.txt') as workfile:
-        read_data = workfile.read()
-        data = read_data.split()
-        print(data)
-        for word1 in list:
-            for word2 in data:
-                if word1 == word2:
-                    return data.remove(list)
-                    print(remove_stopwords)
-for word in match_pattern:
-    count = frequency.get(word, 0)  #word is key, value = 0
-    frequency[word] = count + 1
-frequency_list = frequency.keys()
-for words in frequency_list:
-    print(words,frequency[words])
+STOP_WORDS = [
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
+    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
+    'will', 'with'
+] #relies on lists of words
+def print_word_freq(): #the main function
+    file_name = input("Enter a song")
+    """Read in `file` and print out the frequency of words in that file."""
+    # Your code will go here. You can write additional functions if you want, and call them inside this function.
+    # first open the file
+    with open(file_name,"r") as f:
+        text_string = f.read().lower()
+    frequency = {}
+    match_pattern = re.findall(r'\b[a-z]{1,15}\b',text_string)
+    for word in match_pattern:
+        count = frequency.get(word, 0)  #word is key, value = 0
+        frequency[word] = count + 1
+    frequency_list = frequency.keys()
+    print(frequency)
+    for words in frequency_list:
+        print(words,frequency[words])
+# This is an "incantation." You will not see it very often, and it needs to be here to be able to pass file names as arguments.
+if __name__ == "__main__":
+    import argparse
+    from pathlib import Path
+    print_word_freq()  #calling on each file
